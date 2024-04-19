461863d13f759c5fd4ab19397de0132e0a25969a
esrAA312 <esraayahya312@gmail.com>
2024-04-19 03:57:13 -0700

    done

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,29 @@
 #!/usr/bin/python3
-"""
-Start Flask application
-"""
+"""starts a Flask web application"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
 
-def Hiesraa:
-    """Returns Hello HBNB!"""
+@app.route("/", strict_slashes=False)
+def Hieseaa():
+    """Display Hello HBNB!"""
     return 'Hello HBNB!'
 
 
+@app.route("/hbnb", strict_slashes=False)
 def HB():
-    """Returns HBNB"""
+    """Display HBNB"""
     return 'HBNB'
 
 
+@app.route('/c/<text>')
 def CC(text):
-    """Display “C ” followed by the value of the text variable"""
+    """Return reformatted text
+    """
     return 'C ' + text.replace('_', ' ')
 
-# Routes
-
-
-app.add_url_rule('/', 'index', Hiesraa)
-app.add_url_rule('/hbnb', 'hbnb', HB)
-app.add_url_rule('/c/<text>', 'c', CC)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
